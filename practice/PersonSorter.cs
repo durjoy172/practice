b735c3f157b403c2876bac4e0ba8964443250a6d
ABDULLAH DURJOY <abdullahdurjoy71@gmail.com>
2026-04-24 22:24:45 +0600

    took the classes in a seperate cs file

---
+++ practice/PersonSorter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace practice
+{
+    public delegate int Comparison<T>(T x, T y);
+    public class Person
+    {
+        public int Age { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class PersonSorter
+    {
+        public void Sort(Person[] people, Comparison<Person> comparison)
+        {
+            for (int i = 0; i < people.Length - 1; i++)
+            {
+                for (int j = i + 1; j < people.Length; j++)
+                {
+                    if (comparison(people[i], people[j]) > 0)
+                    {
+                        Person temp = people[i];
+                        people[i] = people[j];
+                        people[j] = temp;
+                    }
+
+
+                }
+            }
+        }
+       
+    }
+}
--- practice/Program.cs
+++ practice/Program.cs
@@ -1,32 +1,5 @@
-﻿namespace practice
-{
-    public delegate int Comparison<T>(T x, T y);
-
-
-    public class Person
-    {
-        public int Age { get; set; }
-        public string Name { get; set; }
-    }
-    public class PersonSorter
-    {
-        public void Sort(Person[] people, Comparison<Person> comparison)
-        {
-            for (int i = 0; i < people.Length - 1; i++)
-            {
-                for (int j = i + 1; j < people.Length; j++)
-                {
-                    if (comparison(people[i], people[j]) > 0)
-                    {
-                        Person temp = people[i];
-                        people[i] = people[j];
-                        people[j] = temp;
-                    }
-
-
-                }
-            }
-        }
+﻿namespace practice 
+{ 
         internal class Program
         {
             static void Main(string[] args)
@@ -50,5 +23,5 @@
             }
         }
     }
-}
+
  
